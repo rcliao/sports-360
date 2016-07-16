1ebe5f04ce3a13d62b7a33a06928cc5e6b1a387a
Eric Liao <rcliao01@gmail.com>
2016-07-15 21:58:57 -0700

    add video and view change event

--- server/app.js
+++ server/app.js
@@ -8,10 +8,42 @@ var request = require('request');
 //Initiate http server
 var app = express();
 
-var setTopBoxIP = 'http://10.10.60.135';
+var setTopBoxIP = 'http://10.10.31.22';
+var serverIp = 'http://10.10.31.21:4005';
 
 //Include static HTML in the 'public' directory
 app.use(express.static('public'));
+app.use('/video', express.static('hls-example'));
+
+function changeToVideoView() {
+    request(setTopBoxIP + ':8080/itv/startURL?url=' + serverIp + '/video', function(err1, res1, body1) {
+        if (!err1 && res1.statusCode == 200) {
+
+        } else {
+          console.log('Change to video view does not work.');
+        }
+    });
+}
+
+function backToNormalView() {
+    request(setTopBoxIP + ':8080/itv/startURL?url=' + serverIp, function(err1, res1, body1) {
+        if (!err1 && res1.statusCode == 200) {
+
+        } else {
+          console.log('Back to normal view does not work.');
+        }
+    });
+}
+
+function stopView() {
+  request(setTopBoxIP + ':8080/itv/stopITV', function(err1, res1, body1) {
+      if (!err1 && res1.statusCode == 200) {
+
+      } else {
+        console.log('We cannot stop the itv app.');
+      }
+  });
+}
 
 //Start the http server on port 4005
 var server = app.listen(4005);
@@ -20,8 +52,21 @@ server.listen(4005, function() {
 });
 
 app.get('/health', function(req, res) {
-  console.info('Still alive!');
+  console.log('Still alive!');
   res.status(200).send('work work');
+});
+
+app.get('/api/tv/view/video', function(req, res) {
+  console.log('Changing view to video');
+  changeToVideoView();
+});
+app.get('/api/tv/view', function(req, res) {
+  console.log('Changing view to normal');
+  backToNormalView();
+});
+app.get('/api/tv/view/stop', function(req, res) {
+  console.log('Stopping ITV app');
+  stopView();
 });
 
 app.get('/api/notification', function(req, res) {
@@ -34,7 +79,7 @@ var io = sio.listen(server);
 
 // Define a message handler
 io.on('connection', function(socket) {
-  console.debug('Someone conected');
+  console.log('Someone conected');
 
   socket.on('ping', function(data) {
     socket.emit('pong');
